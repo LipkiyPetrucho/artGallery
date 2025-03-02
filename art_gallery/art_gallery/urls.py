a92873e9799dd9bd88072c3314b9e3908a2055fc
Petr <bang1987@mail.ru>
2025-03-02 23:26:43 +0400

    added robot.txt in nginx.conf

--- art_gallery/art_gallery/urls.py
+++ art_gallery/art_gallery/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.views.static import serve
 
 from artworks.views import home_view
 from sitemap import StaticViewSitemap, PaintingSitemap, PostSitemap
@@ -20,6 +21,7 @@ urlpatterns = [
     path("artworks/", include("artworks.urls")),
     path("blog/", include("blog.urls")),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('google1234567890abc.html', serve, {'document_root': settings.STATIC_ROOT, 'path': 'google1234567890abc.html'}),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
